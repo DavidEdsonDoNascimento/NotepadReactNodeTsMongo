0e03db94422d8cf38caef98aceeef2ddd01f057b
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-06-02 18:06:09 -0300

    Created the standard structure that will be followed in the backend, created the connection with Mongo and made the first route

--- backend/dist/router/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const AnnotationController_1 = require("@controllers/AnnotationController");
-const routes = (0, express_1.Router)();
-exports.routes = routes;
-routes.get('/annotations', AnnotationController_1.AnnotationController.read);
