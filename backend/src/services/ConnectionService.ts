76875c9c018644b9434f8838258d6655ac35118c
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-06-04 22:26:45 -0300

    add .env in project backend

--- backend/src/services/ConnectionService.ts
+++ backend/src/services/ConnectionService.ts
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
-const DB_CONFIG = 'mongodb+srv://usuario:usuario@notepadsample.lwk6sib.mongodb.net/notepad?retryWrites=true&w=majority&appName=NotepadSample';
+import * as dotenv from "dotenv";
+dotenv.config();
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const DB_CONFIG = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@notepadsample.lwk6sib.mongodb.net/notepad?retryWrites=true&w=majority&appName=NotepadSample`;
 class ConnectionService {
   static connect(): void {
     mongoose.connect(DB_CONFIG);
